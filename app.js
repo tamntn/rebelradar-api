8d58efed978f973c27e89af88d0e1ab6278d5e10
Tam Nguyen <tam.nguyen9779@gmail.com>
2018-09-29 11:41:09 -0400

    Minor Update

--- app.js
+++ app.js
@@ -26,12 +26,11 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 var allowCrossDomain = function (req, res, next) {
-  var allowedOrigins = ['http://localhost:3000', 'https://rebelradar.herokuapp.com', 'http://rebelradar.herokuapp.com'];
+  var allowedOrigins = ['http://localhost:3000', 'http://localhost:3001', 'http://rebelradar.herokuapp.com'];
   var origin = req.headers.origin;
   if (allowedOrigins.indexOf(origin) > -1) {
     res.setHeader('Access-Control-Allow-Origin', origin);
   }
-  // res.header('Access-Control-Allow-Origin', "http://localhost:3001, https://app.aquagrow.life");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
